cc3b7a7376a777f2faee059ceaa9a7c962928026
Abhishek <mauryaabhi20051@gmail.com>
2025-09-17 10:46:03 +0530

    LL

---
+++ RemoveDuplicatesFromSortedListll.java
@@ -0,0 +1,41 @@
+public class RemoveDuplicatesFromSortedListll {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode dummyNode = new ListNode(0);
+        dummyNode.next = head;
+        ListNode prev = dummyNode;
+        ListNode curr = head;
+        while (curr != null) {
+            if (curr.next != null && curr.val == curr.next.val) {
+                while (curr.next != null && curr.val == curr.next.val) {
+                    curr = curr.next;
+                }
+                prev.next = curr.next;
+            } else {
+                prev = prev.next;
+            }
+            curr = curr.next;
+        }
+        return dummyNode.next;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
