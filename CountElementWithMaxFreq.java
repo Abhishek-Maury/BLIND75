2e0c4f39804a65369eab6dc709a3f3ca98601722
Abhishek <mauryaabhi20051@gmail.com>
2025-09-23 13:38:06 +0530

    stack

---
+++ CountElementWithMaxFreq.java
@@ -0,0 +1,24 @@
+public class CountElementWithMaxFreq {
+    // POTD
+
+    public int maxFrequencyElements(int[] nums) {
+        int freq[] = new int[101];
+        int maxfreq = 0;
+        for (int num : nums) {
+            freq[num]++;
+            maxfreq = Math.max(maxfreq, freq[num]);
+        }
+        int ans = 0;
+        for (int f : freq) {
+            if (f == maxfreq) {
+                ans += maxfreq;
+            }
+        }
+        return ans;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
